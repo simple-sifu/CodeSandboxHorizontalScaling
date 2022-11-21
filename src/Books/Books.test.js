5d5d0751e80ad5f54418bf392e6b99cf4d182b19
Tommy Han <kattyntommy@gmail.com>
2022-11-21 16:03:06 +0000

    Update 2 files

--- src/Books/Books.test.js
+++ src/Books/Books.test.js
@@ -1,4 +1,20 @@
+import AddBooksPresenter from "./AddBooksPresenter";
+import httpGateway from "../Shared/HttpGateway";
+import Observable from "../Shared/Observable";
+import booksRepository from "./BooksRepository";
+
 describe("add book", () => {
-  // it("should call api", async () => {});
-  // it("should load and reload books", async () => {});
+  it.only("should call api", async () => {
+    jest.clearAllMocks();
+    booksRepository.booksPm = new Observable([]);
+    httpGateway.post = jest.fn();
+    let addBooksPresenter = new AddBooksPresenter();
+    addBooksPresenter.addBook("UFT", "Peter Heard");
+    console.log(httpGateway.post.mock.calls);
+    expect(httpGateway.post).toBeCalledWith(
+      "https://api.logicroom.co/api/pete@logicroom.co/books",
+      { name: "UFT", author: "Peter Heard", ownerId: "pete@logicroom.co" }
+    );
+  });
+  it("should load and reload books", async () => {});
 });
