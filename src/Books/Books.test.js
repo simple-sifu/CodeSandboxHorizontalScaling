88a48dd6255e511d92b6e5454dbe0747034e757f
Tommy Han <kattyntommy@gmail.com>
2022-11-21 19:05:17 +0000

    Update Books.test.js

--- src/Books/Books.test.js
+++ src/Books/Books.test.js
@@ -30,20 +30,40 @@ describe("add book", () => {
     httpGateway.get = jest.fn().mockImplementation((path) => {
       return GetPublicBooksStub();
     });
+    let bookListPresenter = new BookListPresenter();
 
     // act
-    let bookListPresenter = new BookListPresenter();
     await bookListPresenter.load((generatedViewModel) => {
       viewModel = generatedViewModel;
     });
 
-    // assert
+    // assert anchor
     expect(httpGateway.get).toBeCalledWith(
       "https://api.logicroom.co/api/pete@logicroom.co/books"
     );
     expect(viewModel[0].name).toBe("Moby Dick");
-    expect(viewModel[2].name).toBe("Wind in the willows");
     expect(viewModel[4].name).toBe("The Hobbit");
     expect(viewModel.length).toBe(5);
+
+    // rearrange for reload(pivot)
+    jest.clearAllMocks();
+    viewModel = null;
+    const pivotedBookStub = GetPublicBooksStub();
+    pivotedBookStub.result.push(pivotedBookStub.result[2]);
+    httpGateway.get = jest.fn().mockImplementation((path) => {
+      return pivotedBookStub;
+    });
+    httpGateway.post = jest.fn();
+    let addBooksPresenter = new AddBooksPresenter();
+    bookListPresenter = new BookListPresenter();
+
+    // act
+    await addBooksPresenter.addBook("UFT", "Pete Heard");
+
+    // assert with new book
+    expect(viewModel[0].name).toBe("Moby Dick");
+    expect(viewModel[4].name).toBe("The Hobbit");
+    expect(viewModel[5].name).toBe("Wind in the willows");
+    expect(viewModel.length).toBe(6);
   });
 });
